6106865357c4e8145de62b530345ba9bc40c990e
Sunny17082 <sunnyprasad1708@gmail.com>
2025-06-13 12:28:27 +0530

    vercel

--- api/src/controllers/ai.controller.js
+++ api/src/controllers/ai.controller.js
@@ -2,7 +2,7 @@ const { GoogleGenerativeAI } = require("@google/generative-ai");
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-pro" }); // Use the appropriate model name
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
 const handleGetSummary = async (req, res) => {
 	try {
